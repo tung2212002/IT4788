c35a25dcaf2b40a8238e9ed49c5d4006e55793cb
quantq-vbee <quantq.vbee@gmail.com>
2023-12-07 07:28:30 +0700

    Update friendScreen and notificationScreen

--- src/screens/FriendsStack/FriendsScreen.js
+++ src/screens/FriendsStack/FriendsScreen.js
@@ -1,13 +1,53 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { FlatList } from 'react-native';
 import styled from 'styled-components/native';
 
-function FriendsScreen({ navigation }) {
+const friendsData = [
+    //data smaple
+    { id: '1', name: 'Friend 1', profileImage: 'https://picsum.photos/200/300' },
+    { id: '2', name: 'Friend 2', profileImage: 'https://picsum.photos/200/300' },
+    { id: '3', name: 'Friend 3', profileImage: 'https://picsum.photos/200/300' },
+];
+
+const Container = styled.View`
+    flex: 1;
+    padding: 20px;
+`;
+
+const FriendItem = styled.View`
+    flex-direction: row;
+    align-items: center;
+    padding: 10px;
+    margin-bottom: 10px;
+    background-color: #f0f0f0;
+    border-radius: 8px;
+`;
+
+const ProfileImage = styled.Image`
+    width: 50px;
+    height: 50px;
+    border-radius: 25px;
+    margin-right: 10px;
+`;
+
+const FriendName = styled.Text`
+    font-size: 16px;
+    font-weight: bold;
+`;
+
+function FriendScreen() {
+    const renderFriend = ({ item }) => (
+        <FriendItem>
+            <ProfileImage source={{ uri: item.profileImage }} />
+            <FriendName>{item.name}</FriendName>
+        </FriendItem>
+    );
+
     return (
-        <View>
-            <Text>FriendsScreen</Text>
-        </View>
+        <Container>
+            <FlatList data={friendsData} renderItem={renderFriend} keyExtractor={(item) => item.id} />
+        </Container>
     );
 }
 
-export default FriendsScreen;
+export default FriendScreen;
--- src/screens/NotificationStack/NotificationScreen.js
+++ src/screens/NotificationStack/NotificationScreen.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { Text, View } from 'react-native';
 import styled from 'styled-components/native';
+
+const Container = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    background-color: #f0f0f0;
+`;
+
+const NotificationCard = styled.View`
+    width: 90%;
+    background-color: #fff;
+    padding: 20px;
+    margin-bottom: 20px;
+    border-radius: 8px;
+    shadow-color: #000;
+    shadow-opacity: 0.2;
+    shadow-offset: {
+        width: 0;
+        height: 2;
+    }
+    elevation: 2;
+`;
+
+const NotificationText = styled.Text`
+    font-size: 16px;
+`;
 
 function NotificationScreen({ navigation }) {
     return (
-        <View>
-            <Text>NotificationScreen</Text>
-        </View>
+        <Container>
+            <NotificationCard>
+                <NotificationText>A new notification!</NotificationText>
+            </NotificationCard>
+        </Container>
     );
 }
 
